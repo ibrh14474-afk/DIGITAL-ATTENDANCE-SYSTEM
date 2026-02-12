d5d4a9cf827d16751985640558e757cea161679a
ibrh14474-afk <ibrh14474@gmail.com>
2026-02-12 06:36:54 -0800

    Add main.cpp starter file

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Digital Attendance System - EEE227" << endl;
+  cout << "System Initialized Successfully" << endl;
+  return 0;
+}
