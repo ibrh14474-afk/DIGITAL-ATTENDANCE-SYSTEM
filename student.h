7cf2b10279c421b9b6a8ab3a221c40dd9cfceeca
ibrh14474-afk <ibrh14474@gmail.com>
2026-02-21 15:22:49 +0000

    Refactor Student class by modifying access specifiers
    Removed private access specifier, updated constructor parameters, and deleted unused methods for better clarity.

--- student.h
+++ student.h
@@ -2,65 +2,18 @@
 #define STUDENT_H
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <sstream>
-
 using namespace std;
 
 class Student {
-private:
+public:
     string name;
     string indexNumber;
 
-public:
     Student() {}
 
-    Student(string n, string idx) {
+    Student(string n, string i) {
         name = n;
-        indexNumber = idx;
-    }
-
-    string getName() const {
-        return name;
-    }
-
-    string getIndexNumber() const {
-        return indexNumber;
-    }
-
-    void display() const {
-        cout << "Name: " << name
-             << " | Index Number: " << indexNumber << endl;
-    }
-
-    // File operations
-    static void saveToFile(const vector<Student>& students) {
-        ofstream file("students.txt");
-
-        for (const auto& s : students) {
-            file << s.indexNumber << "," << s.name << endl;
-        }
-
-        file.close();
-    }
-
-    static void loadFromFile(vector<Student>& students) {
-        ifstream file("students.txt");
-        string line;
-
-        while (getline(file, line)) {
-            stringstream ss(line);
-            string idx, n;
-
-            getline(ss, idx, ',');
-            getline(ss, n);
-
-            students.push_back(Student(n, idx));
-        }
-
-        file.close();
+        indexNumber = i;
     }
 };
 
